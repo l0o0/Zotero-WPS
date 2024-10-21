103a15d4dd8609584691b74580f0f8ced3424eef
bigben446 <1125858+bigben446@users.noreply.github.com>
2024-10-21 11:05:20 +0800

    兼容最新Win WPS
    在https://github.com/tankwyn/WPS-Zotero基础上，重构项目使兼容wpsjs最新加载项，支持最新版Win WPS，但是还不能支持Linux WPS。

---
+++ main.js
@@ -0,0 +1,6 @@
+// included by index.html
+document.write("<script language='javascript' src='js/ribbon.js'></script>");
+document.write("<script language='javascript' src='js/zclient.js'></script>");
+document.write("<script language='javascript' src='js/tools.js'></script>");
+document.write("<script language='javascript' src='js/wpsif.js'></script>");
+document.write("<script language='javascript' src='version.js'></script>");
