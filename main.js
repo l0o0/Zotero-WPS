fdf2577d6393302b495478d46b8504a621e2a24a
bigben446 <1125858+bigben446@users.noreply.github.com>
2024-10-21 11:01:45 +0800

    删除相关文件
    可用版本在https://github.com/tankwyn/WPS-Zotero

--- main.js
+++
@@ -1,6 +0,0 @@
-// included by index.html
-document.write("<script language='javascript' src='js/ribbon.js'></script>");
-document.write("<script language='javascript' src='js/zclient.js'></script>");
-document.write("<script language='javascript' src='js/tools.js'></script>");
-document.write("<script language='javascript' src='js/wpsif.js'></script>");
-document.write("<script language='javascript' src='version.js'></script>");
